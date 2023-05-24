2b2995c7ffc5a532fc97cd2d9c492ddc2b4fa344
Ocamp09 <campbeo2@miamioh.edu>
2023-05-24 19:46:29 -0400

    base level foundry tests

--- foundry/test/Accounts.t.sol
+++ foundry/test/Accounts.t.sol
@@ -20,20 +20,21 @@ contract AccountsTest is Test {
     // results
     bool res;
 
+    // initialize some variables
     function setUp() public {
          // set this address as the admin address and create instance of the AmbulanceBounties contract
         superAdmin = vm.addr(1);
-        vm.startPrank(superAdmin);
+        vm.prank(superAdmin);
         a = new Accounts();
-        vm.stopPrank();
 
         // addresses to be used for testing
         admin = vm.addr(1);
-        ambulance = vm.addr(1);
-        initiator = vm.addr(1);
-        hospital = vm.addr(1);
+        ambulance = vm.addr(2);
+        initiator = vm.addr(3);
+        hospital = vm.addr(4);
     }
 
+    // test adding, removing, and isAccount functionality for admins
     function testAddIsRemoveAdmin() public {
         // set sender as super admin
         vm.startPrank(superAdmin);
@@ -53,21 +54,75 @@ contract AccountsTest is Test {
         vm.stopPrank();
     }
 
+    // test adding, removing, and isAccount functionality for ambulances
     function testAddIsRemoveAmbulance() public {
-        vm.startPrank(admin);
-        bool res = a.isAmbulance(ambulance);
-        assertEq(res, false);
+        // add new admin
+        vm.prank(superAdmin);
+        a.addAdmin(admin);
+     
+        // test ambulance functions
+        vm.startPrank(superAdmin);
+            // test is function
+            res = a.isAmbulance(ambulance);
+            assertEq(res, false);
 
-        a.addAmbulance(ambulance);
-        res = a.isAmbulance(ambulance);
-        assertEq(res, true);
+            // test add function
+            a.addAmbulance(ambulance);
+            res = a.isAmbulance(ambulance);
+            assertEq(res, true);
 
-        a.removeAmbulance(ambulance);
-        res = a.isAmbulance(ambulance);
-        assertEq(res, false);
+            // test remove function
+            a.removeAmbulance(ambulance);
+            res = a.isAmbulance(ambulance);
+            assertEq(res, false);
+        vm.stopPrank();
+    }
 
-        res = a.isAmbulance(initiator);
-        assertEq(res, false);
+    // test adding, removing, and isAccount functionality for initiators
+    function testAddIsRemoveInitiator() public {
+        // add new admin
+        vm.prank(superAdmin);
+        a.addAdmin(admin);
+     
+        // test initiator functions
+        vm.startPrank(superAdmin);
+            // test is function
+            res = a.isInitiator(initiator);
+            assertEq(res, false);
+
+            // test add function
+            a.addInitiator(initiator);
+            res = a.isInitiator(initiator);
+            assertEq(res, true);
+
+            // test remove function
+            a.removeInitiator(initiator);
+            res = a.isInitiator(initiator);
+            assertEq(res, false);
+        vm.stopPrank();
+    }
+
+    // test adding, removing, and isAccount functionality for hospitals
+    function testAddIsRemoveHospital() public {
+        // add new admin
+        vm.prank(superAdmin);
+        a.addAdmin(admin);
+     
+        // test hospital functions
+        vm.startPrank(superAdmin);
+            // test is function
+            res = a.isHospital(hospital);
+            assertEq(res, false);
+
+            // test add function
+            a.addHospital(hospital);
+            res = a.isHospital(hospital);
+            assertEq(res, true);
+
+            // test remove function
+            a.removeHospital(hospital);
+            res = a.isHospital(hospital);
+            assertEq(res, false);
         vm.stopPrank();
     }
 }
