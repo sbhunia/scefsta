6bf06c129b022952e5ee54ba010762f2bf6f8bd8
Ocamp09 <campbeo2@miamioh.edu>
2023-06-13 21:42:45 -0400

    added deployment for both contracts

--- truffle/migrations/2_deployContracts.js
+++ truffle/migrations/2_deployContracts.js
@@ -3,4 +3,8 @@ const Auctions = artifacts.require("Auctions");
 
 module.exports = function (deployer) {
     deployer.deploy(Accounts);
+    deployer.deploy(Auctions, Accounts.address);
+
+    console.log("Accounts contract address:", Accounts.address);
+    console.log("Auctions contract address:", Auctions.address);
 }
