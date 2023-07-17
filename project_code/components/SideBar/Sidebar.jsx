587d74ab890a75c6d4e670baeeaa03bf42b3a2ea
Ocamp09 <campbeo2@miamioh.edu>
2023-07-17 18:06:01 -0400

    sidebar only reflects accessible accounts

--- project_code/components/SideBar/Sidebar.jsx
+++ project_code/components/SideBar/Sidebar.jsx
@@ -1,41 +1,60 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 import {
-    SidebarAdminDataDashbaord,
-    SidebarAdminDataNotifications,
-    SidebarAdminDataSettings
-} from './SidebarAdminData.jsx';
-import styles from '../../styles/Sidebar.module.css';
-import Link from 'next/link';
-import BackgroundBlobWhite from './BackgroundBlobWhite';
-import BackgroundBlobAzure from './BackgroundBlobAzure';
-
+  SidebarAdminDataDashbaord,
+  SidebarAdminDataSettings,
+  getSideBarData,
+} from "./SidebarAdminData.jsx";
+import styles from "../../styles/Sidebar.module.css";
+import Link from "next/link";
+import BackgroundBlobWhite from "./BackgroundBlobWhite";
+import BackgroundBlobAzure from "./BackgroundBlobAzure";
 
 export default function Sidebar() {
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [isHospital, setIsHospital] = useState(false);
+  const [isPolice, setIsPolice] = useState(false);
+  const [isAmbulance, setIsAmbulance] = useState(false);
+  const [sidebarAdminDataDashboard, setSidebarAdminDataDashboard] = useState(
+    SidebarAdminDataDashbaord
+  );
 
-    return (
-        <div className={styles.sidebarStyles}>
-            <div className={styles.blobs}>
-                <BackgroundBlobWhite />
-                <BackgroundBlobAzure />
-            </div>
-            <div className={styles.sidebarWrapper}>
-                <div className={styles.sidebarMenu}>
-                    <h3 className={styles.sidebarTitle}>Role Navigation</h3>
-                    <ul className={styles.sidebarList}>
-                        {SidebarAdminDataDashbaord.map((item, index) => {
-                            return (
-                                <Link href={item.path}>
-                                    <li key={index} className={styles.flexListItem}>
-                                        <h6 className={styles.sidebarIcon} > {item.icon} </h6>
-                                        <h6 className={styles.sidebarListItem}>{item.title}</h6>
-                                    </li>
-                                </Link>
-                            );
-                        })}
-                    </ul>
-                </div>
+  useEffect(() => {
+    setIsAdmin(JSON.parse(sessionStorage.getItem("isAdmin")));
+    setIsHospital(JSON.parse(sessionStorage.getItem("isHospital")));
+    setIsPolice(JSON.parse(sessionStorage.getItem("isPolice")));
+    setIsAmbulance(JSON.parse(sessionStorage.getItem("isAmbulance")));
+  });
+  console.log("side", SidebarAdminDataDashbaord);
+  useEffect(() => {
+    setSidebarAdminDataDashboard(
+      getSideBarData(isAdmin, isHospital, isPolice, isAmbulance)
+    );
+  }, [isAdmin, isHospital, isPolice, isAmbulance]);
 
-                <div className={styles.sidebarMenu}>
+  return (
+    <div className={styles.sidebarStyles}>
+      <div className={styles.blobs}>
+        <BackgroundBlobWhite />
+        <BackgroundBlobAzure />
+      </div>
+      <div className={styles.sidebarWrapper}>
+        <div className={styles.sidebarMenu}>
+          <h3 className={styles.sidebarTitle}>Role Navigation</h3>
+          <ul className={styles.sidebarList}>
+            {sidebarAdminDataDashboard.map((item, index) => {
+              return (
+                <Link href={item.path}>
+                  <li key={index} className={styles.flexListItem}>
+                    <h6 className={styles.sidebarIcon}> {item.icon} </h6>
+                    <h6 className={styles.sidebarListItem}>{item.title}</h6>
+                  </li>
+                </Link>
+              );
+            })}
+          </ul>
+        </div>
+
+        {/* <div className={styles.sidebarMenu}>
                     <h3 className={styles.sidebarTitle}>Notifications</h3>
                     <ul className={styles.sidebarList}>
                         {SidebarAdminDataNotifications.map((item, index) => {
@@ -47,24 +66,24 @@ export default function Sidebar() {
                             );
                         })}
                     </ul>
-                </div>
+                </div> */}
 
-                <div className={styles.sidebarMenu}>
-                    <h3 className={styles.sidebarTitle}>Settings</h3>
-                    <ul className={styles.sidebarList}>
-                        {SidebarAdminDataSettings.map((item, index) => {
-                            return (
-                                <Link href={item.path}>
-                                    <li key={index} className={styles.flexListItem}>
-                                        <h6 className={styles.sidebarIcon} > {item.icon} </h6>
-                                        <h6 className={styles.sidebarListItem}>{item.title}</h6>
-                                    </li>
-                                </Link>
-                            );
-                        })}
-                    </ul>
-                </div>
-            </div>
+        <div className={styles.sidebarMenu}>
+          <h3 className={styles.sidebarTitle}>Settings</h3>
+          <ul className={styles.sidebarList}>
+            {SidebarAdminDataSettings.map((item, index) => {
+              return (
+                <Link href={item.path}>
+                  <li key={index} className={styles.flexListItem}>
+                    <h6 className={styles.sidebarIcon}> {item.icon} </h6>
+                    <h6 className={styles.sidebarListItem}>{item.title}</h6>
+                  </li>
+                </Link>
+              );
+            })}
+          </ul>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
--- project_code/components/SideBar/SidebarAdminData.jsx
+++ project_code/components/SideBar/SidebarAdminData.jsx
@@ -3,78 +3,106 @@ import * as MdIcons from "react-icons/md";
 import * as FaIcons from "react-icons/fa";
 import * as BiIcons from "react-icons/bi";
 import * as CgIcons from "react-icons/cg";
-import * as Constants from '../../pages/constants';
-
+import * as Constants from "../../pages/constants";
 
 // 'title' provides text for each tab
 // 'path' provides a link/reference to a page
 // 'icon' provides an icon for each tab
-// 'cName' is the CSS class name for that object 
-
+// 'cName' is the CSS class name for that object
 
 // for the "Dashboard" section of the sidebar
+export const getSideBarData = (isAdmin, isHospital, isPolice, isAmbulance) => {
+  const displayArr = [];
+  if (isAdmin) {
+    displayArr.push({
+      title: Constants.ADMINISTRATOR,
+      path: Constants.DevAdminURL,
+      icon: <FaIcons.FaUserCog />,
+      cName: "flexListItem",
+    });
+  }
+  if (isHospital) {
+    displayArr.push({
+      title: Constants.HOSPITAL,
+      path: Constants.DevHospitalURL,
+      icon: <FaIcons.FaHospitalSymbol />,
+      cName: "flexListItem",
+    });
+  }
+  if (isPolice) {
+    displayArr.push({
+      title: Constants.POLICE,
+      path: Constants.DevPoliceURL,
+      icon: <FaIcons.FaBuilding />,
+      cName: "flexListItem",
+    });
+  }
+  if (isAmbulance) {
+    displayArr.push({
+      title: Constants.AMBULANCE,
+      path: Constants.DevAmbulanceURL,
+      icon: <FaIcons.FaAmbulance />,
+      cName: "flexListItem",
+    });
+  }
+  return displayArr;
+};
+
 export const SidebarAdminDataDashbaord = [
-    // General dashboard page
-    {
-        title: Constants.DASHBOARD,
-        path: Constants.DevHomeURL,
-        icon: <FaIcons.FaHome />,
-        cName: 'flexListItem'
-    },
-    {
-        title: Constants.ADMINISTRATOR,
-        path: Constants.DevAdminURL,
-        icon: <FaIcons.FaUserCog />,
-        cName: 'flexListItem'
-    },
-    {
-        title: Constants.HOSPITAL,
-        path: Constants.DevHospitalURL,
-        icon: <FaIcons.FaHospitalSymbol />,
-        cName: 'flexListItem'
-    },
-    {
-        title: Constants.POLICE,
-        path: Constants.DevPoliceURL,
-        icon: <FaIcons.FaBuilding />,
-        cName: 'flexListItem'
-    },
-    {
-        title: Constants.AMBULANCE,
-        path: Constants.DevAmbulanceURL,
-        icon: <FaIcons.FaAmbulance />,
-        cName: 'flexListItem'
-    },
-]
+  {
+    title: Constants.ADMINISTRATOR,
+    path: Constants.DevAdminURL,
+    icon: <FaIcons.FaUserCog />,
+    cName: "flexListItem",
+  },
+  {
+    title: Constants.HOSPITAL,
+    path: Constants.DevHospitalURL,
+    icon: <FaIcons.FaHospitalSymbol />,
+    cName: "flexListItem",
+  },
+  {
+    title: Constants.POLICE,
+    path: Constants.DevPoliceURL,
+    icon: <FaIcons.FaBuilding />,
+    cName: "flexListItem",
+  },
+  {
+    title: Constants.AMBULANCE,
+    path: Constants.DevAmbulanceURL,
+    icon: <FaIcons.FaAmbulance />,
+    cName: "flexListItem",
+  },
+];
 
 // for the "Notifications" section of the sidebar
 export const SidebarAdminDataNotifications = [
-    {
-        title: "Messages",
-        path: "#",
-        icon: <BiIcons.BiMessageDetail />,
-        cName: 'flexListItem'
-    },
-    {
-        title: "Reports",
-        path: "#",
-        icon: <MdIcons.MdReportProblem />,
-        cName: 'flexListItem'
-    },
-]
+  {
+    title: "Messages",
+    path: "#",
+    icon: <BiIcons.BiMessageDetail />,
+    cName: "flexListItem",
+  },
+  {
+    title: "Reports",
+    path: "#",
+    icon: <MdIcons.MdReportProblem />,
+    cName: "flexListItem",
+  },
+];
 
 // for the "Settings" section of the sidebar
 export const SidebarAdminDataSettings = [
-    {
-        title: Constants.PROFILE,
-        path: Constants.DevProfileURL,
-        icon: <CgIcons.CgProfile />,
-        cName: 'flexListItem'
-    },
-    {
-        title: Constants.ABOUT,
-        path: Constants.DevAboutURL,
-        icon: <FaIcons.FaInfo />,
-        cName: 'flexListItem'
-    },
-]
+  {
+    title: Constants.PROFILE,
+    path: Constants.DevProfileURL,
+    icon: <CgIcons.CgProfile />,
+    cName: "flexListItem",
+  },
+  {
+    title: Constants.ABOUT,
+    path: Constants.DevAboutURL,
+    icon: <FaIcons.FaInfo />,
+    cName: "flexListItem",
+  },
+];
