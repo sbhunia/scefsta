4debb97aa6727594bc289ffcf19d499a11f322e2
Ocamp09 <campbeo2@miamioh.edu>
2023-08-26 13:24:05 -0400

    update index.js for temp backend api

--- backend-api/index.js
+++ backend-api/index.js
@@ -1,10 +1,11 @@
 const dotenv = require("dotenv");
+dotenv.config();
+
 const express = require("express");
 
-dotenv.config();
 const app = express();
 
-app.get("endpoint-1", (res, req, next) => {
+app.get("/endpoint-1", (req, res, next) => {
   res.status(200).json({
     success: true,
     data: {
@@ -13,5 +14,14 @@ app.get("endpoint-1", (res, req, next) => {
   });
 });
 
+app.get("/endpoint-2", (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    data: {
+      message: "Hello from endpoint 2",
+    },
+  });
+});
+
 const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => console.log(`Server is running on this port: ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on this port ${PORT}`));
